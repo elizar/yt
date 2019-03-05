e46d73fd23c88d044304b10e971d593a010cb683
Elizar Pepino <jupenz@gmail.com>
2019-03-06 00:10:02 +0800

    update youtube endpoint to show hd/4k videos from results

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 )
 
-const YTURL = "http://www.youtube.com/get_video_info?video_id=%s&el=embedded&ps=default&eurl=&gl=US&hl=en"
+const YTURL = "http://www.youtube.com/get_video_info?video_id=%s&el=vevo&fmt=18&asv=2&hd=1"
 
 func main() {
 	port := ":" + os.Getenv("PORT")
